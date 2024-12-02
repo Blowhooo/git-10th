a5fd1b296f5bb0e2cab3adf4b15fb217079c6219
Blowhooo <blowhooo@gmail.com>
2024-12-03 00:23:46 +0900

    열번째 연습

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,45 @@
+'use strict';
+
+class UserStorage{
+  static #users = {
+    id : ["login1", "login2", "login3"],
+    psword : ["1234", "5678", "9101"],
+    name : ["테스터1", "테스터2", "테스터3"]
+  }
+
+  static getUsers(...fields){
+    const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if(users.hasOwnProperty(field)){
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  };
+
+  static getUserInfo(id){
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const userKeys = Object.keys(users);
+    const userInfo = userKeys.reduce((newUsers, info) => {
+      newUsers[info] = users[info][idx];
+      return newUsers;
+    }, {});
+    return userInfo;
+  };
+
+  static save(userInfo){
+    const users = this.#users;
+    if(users.id.includes(userInfo.id)){
+      return false;
+    }
+    users.id.push(userInfo.id);
+    users.psword.push(userInfo.psword);
+    users.name.push(userInfo.name);
+    console.log(users)
+    return {success: true, msg: "회원가입이 완료되었습니다"}
+  }
+}
+
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,18 +1,30 @@
 'use strict';
 
+const User = require('../../models/User');
+
 const output = {
   home : (req, res) => {
     res.render('home/index');
   },
   login : (req, res) => {
     res.render('home/login');
+  },
+  register : (req, res) => {
+    res.render('home/register');
   }
 }
 
 const process = {
   login : (req, res) => {
-    console.log(req.body)
-  }
+    const users = new User(req.body);
+    const response = users.login();
+    return res.json(response);
+  },
+  register : (req, res) => {
+    const users = new User(req.body);
+    const response = users.register();
+    return res.json(response);
+  },
 }
 
 module.exports = {
